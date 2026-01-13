4f207266e88cc1501f55127cc312e104d1eab2f7
Farah Saleem <fs8578058@gmail.com>
2026-01-13 02:54:25 -0800

    add some code of line

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const navLinks = document.querySelector("#nav-links");
 const menuBtnIcon = document.querySelector("i");
 
 
-menuBtn.addEventListener("click", ()=> {
+menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     const isOpen = navLinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", 
+    menuBtnIcon.setAttribute("class",
         isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
 
     );
@@ -16,3 +16,42 @@ navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
 })
+
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header-img img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header-content h2", {
+    ...scrollRevealOption,
+     delay: 500,
+});
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+     delay: 1000,
+});
+
+ScrollReveal().reveal(".header-content p", {
+    ...scrollRevealOption,
+     delay: 1500,
+});
+
+ScrollReveal().reveal(".header-btn", {
+    ...scrollRevealOption,
+     delay: 2000,
+});
+
+ScrollReveal().reveal(".header-socials li", {
+    ...scrollRevealOption,
+     delay: 2500,
+     Interval : 500,
+});
